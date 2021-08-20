d664119d10c5e526d533674a2d5febd8e7b2d9f6
Bryan Medina <bmedinacordero2@gmail.com>
2021-08-20 00:37:05 -0400

    caulculando la moda en javascript

---
+++ moda.js
@@ -0,0 +1,46 @@
+// const lista1 = [
+//     1,
+//     2,
+//     3,
+//     1,
+//     2,
+//     3,
+//     4,
+//     2,
+//     2,
+//     2,
+//     1
+// ];
+
+const listaCount = {};
+
+
+
+function calcularModa(lista) {
+    
+    lista.map(
+        function (elemento) {
+            if (listaCount[elemento]) {
+                listaCount[elemento] +=1;
+                
+            } else{
+                listaCount[elemento] = 1;
+    
+            }
+        }
+    )
+
+
+
+const listaArray = Object.entries(listaCount).sort(
+    function (elementoA, elementoB) {
+       return elementoA[1] - elementoB[1];
+        
+    }
+);
+
+const moda = listaArray[listaArray.length - 1];
+return moda;
+
+}
+
